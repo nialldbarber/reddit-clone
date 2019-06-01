f657d8e28d20893ede26db519455fb918be4feff
Niall Barber <niall.barber@MBP-0093.home>
2019-06-01 21:05:38 +0100

    Added content and sidebar

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Header from "components/Header";
+import Sidebar from "components/Sidebar";
 
 export default function Homepage() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <div className="content">
+          <p>im the content</p>
+        </div>
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <div>
+      <h3>Trending Subreddits</h3>
+    </div>
+  );
+}
